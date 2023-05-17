d65c66c4639aebb7db6afea8165d69d37799a406
nickonyi <nickoluoch1130@gmail.com>
2023-05-17 06:41:59 +0300

    final touches

--- src/menu.js
+++ src/menu.js
@@ -22,9 +22,9 @@ function createMenu() {
     menuHeaderContainer.appendChild(menuTitle);
     menuHeaderContainer.appendChild(menuText);
     menu.appendChild(menuHeaderContainer);
-    menu.appendChild(createMenuItem('Pancakes', '$12.00', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.', menuImage1));
-    menu.appendChild(createMenuItem('Eggs', '$15.00', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.', menuImage2));
-    menu.appendChild(createMenuItem('Onions', '$10.00', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.', menuImage3));
+    menu.appendChild(createMenuItem('Pancakes', '$12.00', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. ', menuImage1));
+    menu.appendChild(createMenuItem('Eggs', '$15.00', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.', menuImage2));
+    menu.appendChild(createMenuItem('Onions', '$10.00', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.', menuImage3));
     return menu;
 }
 
@@ -35,6 +35,9 @@ function createMenuItem(name, price, description, image) {
 
     const menuItemDiv = document.createElement('div');
     menuItemDiv.classList.add('menu__item-div');
+
+    const menuItemHeader = document.createElement('div');
+    menuItemHeader.classList.add('menu__item-header');
 
     const menuItemName = document.createElement('h3');
     menuItemName.classList.add('menu__item-name');
@@ -48,20 +51,22 @@ function createMenuItem(name, price, description, image) {
     menuItemPrice.classList.add('menu__item-price');
     menuItemPrice.textContent = price;
 
-    menuItemDiv.appendChild(menuItemName);
-    menuItemDiv.appendChild(menuItemDecoration);
-    menuItemDiv.appendChild(menuItemPrice);
-
     const menuItemDescription = document.createElement('p');
     menuItemDescription.classList.add('menu__item-description');
     menuItemDescription.textContent = description;
+
+    menuItemHeader.appendChild(menuItemName);
+    menuItemHeader.appendChild(menuItemDecoration);
+    menuItemHeader.appendChild(menuItemPrice);
+    menuItemDiv.appendChild(menuItemHeader);
+    menuItemDiv.appendChild(menuItemDescription);
+
 
     const menuItemImage = document.createElement('img');
     menuItemImage.classList.add('menu__item-image');
     menuItemImage.src = image;
 
     menuItem.appendChild(menuItemDiv);
-    menuItem.appendChild(menuItemDescription);
     menuItem.appendChild(menuItemImage);
 
     return menuItem;
